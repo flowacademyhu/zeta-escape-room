93c4b239404b84a6ef2d1a17c27ee54965ba484f
Martin Dezsi <dezsimartin@gmail.com>
2019-06-24 16:02:28 +0200

    main.js re

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 let generate2d = require('./generate2d');
 let twArray = require('./labirint');
+let move = require('./move');
 
 console.log('szasztok');
 let array = generate2d(20, 20);
 console.log(twArray(array));
+let coord = '1 1';
+
+console.log(move(array, coord));
