f4a2784f4aae8f08c5c027a6718d2fc1179dc617
Molsagari92 <gabi.sajti@gmail.com>
2019-06-25 14:33:08 +0200

    upd

--- move.js
+++ move.js
@@ -1,41 +1,42 @@
-let axel = require('axel');
+let term = require('terminal-kit').terminal;
 const move = (arr, where) => {
   let readline = require('readline-sync');
   let a = readline.keyIn('Press a key! ');
-  let place = where.split(' ');
-  let x = Number(place[0]);
-  let y = Number(place[1]);
+  let x = Number(where[0]);
+  let y = Number(where[1]);
   if (a === 'w') {
     if (x > 2 && arr[x - 1][y !== 1]) {
       x = x - 1;
-      // axel.box(x, y, 2, 1);
+      term.bgCyan(arr[x][y] + ' ');
     } else {
       console.log('Invalid movement!');
     }
   } else if (a === 's') {
     if (x < arr.length - 2 && arr[x + 1][y] !== 1) {
       x = x + 1;
-      // axel.box(x, y, 2, 1);
+      term.bgCyan(arr[x][y] + ' ');
     } else {
       console.log('Invalid movement!');
     }
   } else if (a === 'd') {
     if (y < arr[x].length - 4 && arr[x][y + 1] !== 1) {
       y = y + 2;
-      // axel.box(x, y, 2, 1);
+      term.bgCyan(arr[x][y] + ' ');
     } else {
       console.log('Invalid movement!');
     }
   } else if (a === 'a') {
     if (y > 4 && arr[x][y - 1] !== 1) {
       y = y - 2;
-      // axel.box(x, y, 2, 1);
+      term.bgCyan(arr[x][y] + ' ');
     } else {
       console.log('Invalid movement!');
     }
   }
-  let result = x + ' ' + y;
-  return result;
+
+  where[0] = x;
+  where[1] = y;
+  return where;
 };
 
 module.exports = move;
