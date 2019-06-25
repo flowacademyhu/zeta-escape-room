ef00873811c989338ae477cf55f25cc3da089867
Martin Dezsi <dezsimartin@gmail.com>
2019-06-25 10:31:43 +0200

    validate modified

--- validate.js
+++ validate.js
@@ -1,11 +1,24 @@
-const validate = (string) => {
+
+const validate = (str) => {
+  let readline = require('readline-sync');
   let boolean = false;
-  let password = 'cica1';
-  if (string === password) {
-    boolean = true;
-    return boolean;
-  } else {
-    return boolean;
+  let life = 3;
+  console.log('Please enter the password to escape. Be careful, you have only 3 opportunity to leave the area.');
+  while (life > 0) {
+    str = readline.question();
+    let password = 'cica1';
+    console.clear();
+    if (str === password) {
+      boolean = true;
+      console.log('Congratulations. You are free to leave.');
+      return boolean;
+    } else {
+      if (life === 1) {
+        return boolean;
+      }
+      life--;
+      console.log('Invalid password. You have ' + life + ' more try.');
+    }
   }
 };
 
