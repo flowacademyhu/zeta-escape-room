ca19b7ed4b54fbb019c091a70da9f085b1488598
Martin Dezsi <dezsimartin@gmail.com>
2019-06-25 14:18:23 +0200

    up

--- main.js
+++ main.js
@@ -2,9 +2,23 @@ let generate2d = require('./generate2d');
 let twArray = require('./labirint');
 let move = require('./move');
 let validate = require('./validate');
+var term = require('terminal-kit').terminal;
 
 let array = generate2d(28, 28);
-console.log(twArray(array));
+array = twArray(array);
+
+for (let i = 0; i < array.length; i++) {
+  for (let j = 0; j < array[i].length; j++) {
+    if (array[i][j] === 1) {
+      term.bgWhite(array[i][j] + ' ');
+    } else {
+      process.stdout.write('0' + ' ');
+    }
+  }
+  console.log();
+}
+
+// console.log(array);
 // let coord = '1 1';
 
 // console.log(move(array, coord));
