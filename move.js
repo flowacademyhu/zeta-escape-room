73e526954e2c56b1ec9905784b0c837f8f71c03e
Molsagari92 <gabi.sajti@gmail.com>
2019-06-25 15:09:46 +0200

    upd

--- move.js
+++ move.js
@@ -1,34 +1,38 @@
 let term = require('terminal-kit').terminal;
-const move = (arr, where) => {
-  let readline = require('readline-sync');
-  let a = readline.keyIn('Press a key! ');
+let readline = require('readline-sync');
+let a = readline.keyIn('Press a key! ');
+const move = (arr, where, a) => {
   let x = Number(where[0]);
   let y = Number(where[1]);
   if (a === 'w') {
-    if (x > 2 && arr[x - 1][y !== 1]) {
+    if (x > 2 && arr[x - 1][y] !== 1) {
+      arr[x][y] = 0;
       x = x - 1;
-      arr[x][y] = term.bgCyan(arr[x][y] + ' ');
+      arr[x][y] = x;
     } else {
       console.log('Invalid movement!');
     }
   } else if (a === 's') {
     if (x < arr.length - 2 && arr[x + 1][y] !== 1) {
+      arr[x][y] = 0;
       x = x + 1;
-      arr[x][y] = term.bgCyan(arr[x][y] + ' ');
+      arr[x][y] = x;
     } else {
       console.log('Invalid movement!');
     }
   } else if (a === 'd') {
     if (y < arr[x].length - 4 && arr[x][y + 1] !== 1) {
+      arr[x][y] = 0;
       y = y + 2;
-      arr[x][y] = term.bgCyan(arr[x][y] + ' ');
+      arr[x][y] = x;
     } else {
       console.log('Invalid movement!');
     }
   } else if (a === 'a') {
     if (y > 4 && arr[x][y - 1] !== 1) {
+      arr[x][y] = 0;
       y = y - 2;
-      arr[x][y] = term.bgCyan(arr[x][y] + ' ');
+      arr[x][y] = x;
     } else {
       console.log('Invalid movement!');
     }
@@ -36,7 +40,7 @@ const move = (arr, where) => {
 
   where[0] = x;
   where[1] = y;
-  return [where, arr];
+  return where;
 };
 
 module.exports = move;
