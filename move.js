04eca1865457caeca48bb888c13d45e8c6816d83
Molsagari92 <gabi.sajti@gmail.com>
2019-06-25 10:49:04 +0200

    up

--- move.js
+++ move.js
@@ -8,28 +8,28 @@ const move = (arr, where) => {
   if (a === 'w') {
     if (x > 2 && arr[x - 1][y !== 1]) {
       x = x - 1;
-      axel.box(x, y, 2, 1);
+      // axel.box(x, y, 2, 1);
     } else {
       console.log('Invalid movement!');
     }
   } else if (a === 's') {
     if (x < arr.length - 2 && arr[x + 1][y] !== 1) {
       x = x + 1;
-      axel.box(x, y, 2, 1);
+      // axel.box(x, y, 2, 1);
     } else {
       console.log('Invalid movement!');
     }
   } else if (a === 'd') {
     if (y < arr[x].length - 4 && arr[x][y + 1] !== 1) {
       y = y + 2;
-      axel.box(x, y, 2, 1);
+      // axel.box(x, y, 2, 1);
     } else {
       console.log('Invalid movement!');
     }
   } else if (a === 'a') {
     if (y > 4 && arr[x][y - 1] !== 1) {
       y = y - 2;
-      axel.box(x, y, 2, 1);
+      // axel.box(x, y, 2, 1);
     } else {
       console.log('Invalid movement!');
     }
