83e5685cd79e07f0abb5ca5251ba76bcfe7f281b
Molsagari92 <gabi.sajti@gmail.com>
2019-06-26 16:03:33 +0200

    upd

--- trap.js
+++ trap.js
@@ -1,12 +1,11 @@
 const trapGen = (array) => {
-  let n=0;
-    while (n<3) {
-    let a = Math.floor(Math.random()*array.length);
-    let b = Math.floor(Math.random()*array[a].length);
-    if (Number(array[a][b].value)===0) {
-        array[a][b].value='T';
-        n++
+  let n = 0;
+  while (n < 3) {
+    let a = Math.floor(Math.random() * array.length);
+    let b = Math.floor(Math.random() * array[a].length);
+    if (Number(array[a][b].value) === 0) {
+      array[a][b].value = 'T';
+      n++;
     }
-}
-}
+  }
 };
