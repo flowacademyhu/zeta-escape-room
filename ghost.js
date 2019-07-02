47d43dfddda59d3cb45b09ea29e22dc1488fb600
Molsagari92 <gabi.sajti@gmail.com>
2019-07-02 11:47:05 +0200

    upd

--- ghost.js
+++ ghost.js
@@ -3,8 +3,9 @@ const ghostSee = (x, y) => {
     if (maze[x - 1][y].value === 'x') {
       let mx = x - 1;
       let my = y;
+      return true;
     } else {
-      see(x - 1, y);
+      ghostSee(x - 1, y);
     }
   } else {
     return false;
@@ -14,7 +15,7 @@ const ghostSee = (x, y) => {
       let mx = x + 1;
       let my = y;
     } else {
-      see(x + 1, y);
+      ghostSee(x + 1, y);
     }
   } else {
     return false;
@@ -24,7 +25,7 @@ const ghostSee = (x, y) => {
       let mx = x;
       let my = y - 1;
     } else {
-      see(x, y - 1);
+      ghostSee(x, y - 1);
     }
   } else {
     return false;
@@ -35,53 +36,51 @@ const ghostSee = (x, y) => {
       let mx = x;
       let my = y + 1;
     } else {
-      see(x, y + 1);
+      ghostSee(x, y + 1);
     }
   } else {
     return false;
   }
 };
 
-const ghostMove= (x, y) => {
-    let x = Number(where[0]);
-    let y = Number(where[1]);
-    let a= Math.floor(Math.random()*4)+1
-  
-    if (a === 1) { // fel
-      if (x > 1 && Number(arr[x - 1][y].value) !== 1) {
-        arr[x][y].value = '0';
-        x = x - 1;
-        arr[x][y].value = 'g';
-      } else {
-        ghostMove(x, y);
-      }
-    } else if (a === 2) { // le
-      if (x < arr.length - 1 && Number(arr[x + 1][y].value) !== 1) {
-        arr[x][y].value = '0';
-        x = x + 1;
-        arr[x][y].value = 'g';
-      } else {
-        ghostMove(x, y);
-      }
-    } else if (a === 3) { // jobbra
-        if (y < arr[x].length - 1 && Number(arr[x][y + 1].value) !== 1) {
-        arr[x][y].value = '0';
-        y = y + 1;
-        arr[x][y].value = 'g';
-      } else {
-        ghostMove(x, y);
-      }
-    } else if (a === 4) { // balra
-      if (y > 1 && Number(arr[x][y - 1].value) !== 1) {
-        arr[x][y].value = '0';
-        y = y - 1;
-        arr[x][y].value = 'g';
-      } else {
-        ghostMove(x, y);
-      }
+const ghostMove = (x, y) => {
+  let a = Math.floor(Math.random() * 4) + 1;
+
+  if (a === 1) { // fel
+    if (x > 1 && Number(maze[x - 1][y].value) !== 1) {
+      maze[x][y].value = '0';
+      x = x - 1;
+      maze[x][y].value = 'g';
+    } else {
+      ghostMove(x, y);
     }
-    return {x, y};
-  };
-  
+  } else if (a === 2) { // le
+    if (x < maze.length - 1 && Number(maze[x + 1][y].value) !== 1) {
+      maze[x][y].value = '0';
+      x = x + 1;
+      maze[x][y].value = 'g';
+    } else {
+      ghostMove(x, y);
+    }
+  } else if (a === 3) { // jobbra
+    if (y < maze[x].length - 1 && Number(maze[x][y + 1].value) !== 1) {
+      maze[x][y].value = '0';
+      y = y + 1;
+      maze[x][y].value = 'g';
+    } else {
+      ghostMove(x, y);
+    }
+  } else if (a === 4) { // balra
+    if (y > 1 && Number(maze[x][y - 1].value) !== 1) {
+      maze[x][y].value = '0';
+      y = y - 1;
+      maze[x][y].value = 'g';
+    } else {
+      ghostMove(x, y);
+    }
+  }
+  return { x, y };
+};
+
 module.exports = ghostSee;
 module.exports = ghostMove;
