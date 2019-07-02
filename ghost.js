52863dc714ac08ecd0aca624074a5557d5a04ec5
Molsagari92 <gabi.sajti@gmail.com>
2019-07-02 11:32:28 +0200

    ghost base

---
+++ ghost.js
@@ -0,0 +1,87 @@
+const ghostSee = (x, y) => {
+  if (maze[x - 1][y].value !== '1') {
+    if (maze[x - 1][y].value === 'x') {
+      let mx = x - 1;
+      let my = y;
+    } else {
+      see(x - 1, y);
+    }
+  } else {
+    return false;
+  }
+  if (maze[x + 1][y].value !== '1') {
+    if (maze[x + 1][y].value === 'x') {
+      let mx = x + 1;
+      let my = y;
+    } else {
+      see(x + 1, y);
+    }
+  } else {
+    return false;
+  }
+  if (maze[x][y - 1].value !== '1') {
+    if (maze[x][y - 1].value === 'x') {
+      let mx = x;
+      let my = y - 1;
+    } else {
+      see(x, y - 1);
+    }
+  } else {
+    return false;
+  }
+
+  if (maze[x][y + 1].value !== '1') {
+    if (maze[x][y + 1].value === 'x') {
+      let mx = x;
+      let my = y + 1;
+    } else {
+      see(x, y + 1);
+    }
+  } else {
+    return false;
+  }
+};
+
+const ghostMove= (x, y) => {
+    let x = Number(where[0]);
+    let y = Number(where[1]);
+    let a= Math.floor(Math.random()*4)+1
+  
+    if (a === 1) { // fel
+      if (x > 1 && Number(arr[x - 1][y].value) !== 1) {
+        arr[x][y].value = '0';
+        x = x - 1;
+        arr[x][y].value = 'g';
+      } else {
+        ghostMove(x, y);
+      }
+    } else if (a === 2) { // le
+      if (x < arr.length - 1 && Number(arr[x + 1][y].value) !== 1) {
+        arr[x][y].value = '0';
+        x = x + 1;
+        arr[x][y].value = 'g';
+      } else {
+        ghostMove(x, y);
+      }
+    } else if (a === 3) { // jobbra
+        if (y < arr[x].length - 1 && Number(arr[x][y + 1].value) !== 1) {
+        arr[x][y].value = '0';
+        y = y + 1;
+        arr[x][y].value = 'g';
+      } else {
+        ghostMove(x, y);
+      }
+    } else if (a === 'a') { // balra
+      if (y > 1 && Number(arr[x][y - 1].value) !== 1) {
+        arr[x][y].value = '0';
+        y = y - 1;
+        arr[x][y].value = 'g';
+      } else {
+        ghostMove(x, y);
+      }
+    }
+    return {x, y};
+  };
+  
+module.exports = ghostSee;
+module.exports = ghostMove;
