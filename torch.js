3ae15e46b972151d62ee0be53899f81e53026413
Martin Dezsi <dezsimartin@gmail.com>
2019-07-02 20:03:06 +0200

    torch x+y max range updated

--- torch.js
+++ torch.js
@@ -1,6 +1,7 @@
-const torchGen = (array) => {
+
+const torchGen = (array, n) => {
   let count = 0;
-  while (count < 3) {
+  while (count < n) {
     let rng1 = Math.floor(Math.random() * 27 + 1);
     let rng2 = Math.floor(Math.random() * 27 + 1);
     for (let i = 1; i < array.length - 1; i++) {
@@ -15,4 +16,43 @@ const torchGen = (array) => {
   return array;
 };
 
-module.exports = torchGen;
+const torchUse = (array, where, range) => {
+  let x = Number(where[0]);
+  let y = Number(where[1]);
+  let c = 0;
+  let d = 0;
+  let tx = x - range;
+  let ty = y - range;
+  if (array[x][y].used === true) {
+    for (let i = tx; i <= x + range; i++) {
+      tx = x - range + c;
+      array[tx][y].visibility = true;
+      c++;
+    }
+    for (let j = ty; j <= y + range; j++) {
+      ty = y - range + d;
+      array[x][ty].visibility = true;
+      d++;
+    }
+  }
+};
+
+const torchUpTime = (array, where) => {
+  let x = Number(where[0]);
+  let y = Number(where[1]);
+  let torchLifeTime = 5;
+  if (torchUse()) {
+    if (x > 2 && x < array.length - 2 && y > 2 && y < array.length - 2) {
+      array[x][y].visibility = true;
+      while (torchLifeTime > 5) {
+        torchLifeTime--;
+      }
+    }
+  }
+};
+
+module.exports = {
+  torchGen: torchGen,
+  torchUse: torchUse,
+  torchUpTime: torchUpTime
+};
