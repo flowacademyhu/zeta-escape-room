646cf8b0ca7f06532a4e54f94a9fc4fa5f1a3973
Martin Dezsi <dezsimartin@gmail.com>
2019-07-04 15:49:04 +0200

    main refactor

--- main.js
+++ main.js
@@ -1,49 +1,20 @@
 const main = () => {
-  var term = require('terminal-kit').terminal;
   let readline = require('readline-sync');
   let trapGen = require('./trap');
   let foodGen = require('./food');
   let move = require('./move');
   let gen = require('./maze_new');
   let ghost = require('./ghost');
-
   let torch = require('./torch');
   let vision = require('./vision');
   let maze = [];
 
-  /* const labBackground = (array) => {
-  for (let i = 0; i < array.length; i++) {
-    for (let j = 0; j < array[i].length; j++) {
-      if (Number(array[i][j].value) === 1 && array[i][j].visibility === true) {
-        term.bgWhite('  ');
-      } else if (array[i][j].visibility === false) {
-        process.stdout.write('  ');
-      } else if (array[i][j].value === 'x') {
-        term.bgRed('  ');
-      } else if (array[i][j].value === 'F') {
-        term.bgGreen(' F');
-      } else if (array[i][j].value === 'H') {
-        term.bgBlack(' H');
-      } else if (array[i][j].value === 'T') {
-        term.bgBlue(' T');
-      } else if (array[i][j].value === 'TO') {
-        term.bgYellow('TO');
-      } else if (Number(array[i][j].value) === 0 && array[i][j].visibility === true) {
-        term.bgBlack('  ');
-      }
-    }
-    console.log();
-  }
-  return array;
-};
-*/
-  maze = gen.generate2d(27, 27);
+  maze = gen.generate2d(35, 35);
   maze = gen.fill2d(maze);
   gen.mazeGen(1, 1, maze);
   maze[1][1] = { value: 'x', visibility: true };
   torch.torchGen(maze, 3);
   trapGen(maze);
-  // gen.labBackground(maze);
   let life = { life: 10 };
   let torchLife = { life: 3 };
 
