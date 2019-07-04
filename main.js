c785ceb24c30e1762e8052b2c5b8055b3f425926
Martin Dezsi <dezsimartin@gmail.com>
2019-07-04 15:39:31 +0200

    menu modified

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
-var term = require('terminal-kit').terminal;
-let readline = require('readline-sync');
-let trapGen = require('./trap');
-let foodGen = require('./food');
-let move = require('./move');
-let gen = require('./maze_new');
-let ghost = require('./ghost');
+const main = () => {
+  var term = require('terminal-kit').terminal;
+  let readline = require('readline-sync');
+  let trapGen = require('./trap');
+  let foodGen = require('./food');
+  let move = require('./move');
+  let gen = require('./maze_new');
+  let ghost = require('./ghost');
 
-let torch = require('./torch');
-let vision = require('./vision');
-let maze = [];
+  let torch = require('./torch');
+  let vision = require('./vision');
+  let maze = [];
 
-/* const labBackground = (array) => {
+  /* const labBackground = (array) => {
   for (let i = 0; i < array.length; i++) {
     for (let j = 0; j < array[i].length; j++) {
       if (Number(array[i][j].value) === 1 && array[i][j].visibility === true) {
@@ -36,48 +37,51 @@ let maze = [];
   return array;
 };
 */
-maze = gen.generate2d(27, 27);
-maze = gen.fill2d(maze);
-gen.mazeGen(1, 1, maze);
-maze[1][1] = { value: 'x', visibility: true };
-torch.torchGen(maze, 3);
-trapGen(maze);
-// gen.labBackground(maze);
-let life = { life: 10 };
-let torchLife = { life: 3 };
+  maze = gen.generate2d(27, 27);
+  maze = gen.fill2d(maze);
+  gen.mazeGen(1, 1, maze);
+  maze[1][1] = { value: 'x', visibility: true };
+  torch.torchGen(maze, 3);
+  trapGen(maze);
+  // gen.labBackground(maze);
+  let life = { life: 10 };
+  let torchLife = { life: 3 };
 
-let a;
-let where = [1, 1];
-// console.clear();
+  let a;
+  let where = [1, 1];
+  console.clear();
 
-let gx = maze.length - 2;
-let gy = maze.length - 2;
-let prot = false;
-while (true) {
-  vision.vision(maze, where, torchLife);
+  let gx = maze.length - 2;
+  let gy = maze.length - 2;
+  let prot = false;
+  while (true) {
+    vision.vision(maze, where, torchLife);
 
-  console.log('a');
-  if (prot) {
-    setTimeout(ghost.hidegHost, 30 * 1000);
-    gx = maze.length - 2;
-    gy = maze.length - 2;
+    console.log('a');
+    if (prot) {
+      setTimeout(ghost.hidegHost, 30 * 1000);
+      gx = maze.length - 2;
+      gy = maze.length - 2;
+    }
+    if (ghost.ghostSee(maze, gx, gy)) {
+      ghost.ghostTargeted(maze, gx, gy);
+    } else {
+      console.log('b');
+      let arr = ghost.ghostRandom(maze, gx, gy);
+      gx = arr[0];
+      gy = arr[1];
+    }
+
+    gen.labBackground(maze);
+
+    a = readline.keyIn();
+    console.clear();
+    move(maze, where, a, life);
+
+    if (a === 'q') {
+      process.exit();
+    }
   }
-  if (ghost.ghostSee(maze, gx, gy)) {
-    ghost.ghostTargeted(maze, gx, gy);
-  } else {
-    console.log('b');
-    let arr = ghost.ghostRandom(maze, gx, gy);
-    gx = arr[0];
-    gy = arr[1];
-  }
+};
 
-  gen.labBackground(maze);
-
-  a = readline.keyIn();
-  // console.clear();
-  move(maze, where, a, life);
-
-  if (a === 'q') {
-    process.exit();
-  }
-}
+module.exports = main;
--- menu.js
+++ menu.js
@@ -1,31 +1,31 @@
+let main = require('./main');
 const menu = () => {
   var term = require('terminal-kit').terminal;
   var center = require('center-align');
 
   let menusor = [
     'New Game',
-    'Load Game',
     'Credits',
     'Exit'
 
   ];
-
+  console.clear();
   menusor = center(menusor);
 
   term.singleColumnMenu(menusor, function (error, response) {
     term('\n').eraseLineAfter.green(
     );
     if (response.selectedIndex === 0) {
-      console.log('új játék');
+      main();
     } else if (response.selectedIndex === 1) {
-      console.log('betőt');
-    } else if (response.selectedIndex === 2) {
       console.log('credits');
-    } else if (response.selectedIndex === 3) {
-      process.exit();
+    } else if (response.selectedIndex === 2) {
+      console.log();
     }
     process.exit();
   });
 };
+
+menu();
 
 module.exports = menu;
