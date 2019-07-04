108a2909d4d0c535cd9f43925710c7a7886ac43e
Martin Dezsi <dezsimartin@gmail.com>
2019-07-04 22:24:26 +0200

    manual updated

--- manual.js
+++ manual.js
@@ -1,7 +1,6 @@
-let menu = require('./menu');
-let readline = require('readline-sync');
-
 const manual = () => {
+  let menu = require('./menu');
+  let readline = require('readline-sync');
   console.clear();
   console.log('Read the fkin manual');
   let a = readline.keyIn();
