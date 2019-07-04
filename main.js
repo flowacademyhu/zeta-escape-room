18462eafd9b413f3eec77daa581e2a7388525588
Tibor Krenacs <krenacs.tibor@gmail.com>
2019-07-04 09:32:26 +0200

    main refactored

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
-let trapGen = require('./trap');
-let foodGen = require('./food');
-// let generate2d = require('./generate2d');
-// let twArray = require('./labirint');
-let move = require('./move');
-// let validate = require('./validate');
 var term = require('terminal-kit').terminal;
 let readline = require('readline-sync');
+let trapGen = require('./trap');
+let foodGen = require('./food');
+let move = require('./move');
 let generateLabirynth = require('./labgen');
 let torch = require('./torch');
 let vision = require('./vision');
@@ -21,11 +18,11 @@ const labBackground = (array) => {
       } else if (array[i][j].value === 'x') {
         term.bgRed('  ');
       } else if (array[i][j].value === 'F') {
-        term.bgBlue(' F');
+        term.bgGreen(' F');
       } else if (array[i][j].value === 'H') {
         term.bgBlack(' H');
       } else if (array[i][j].value === 'T') {
-        term.bgGreen(' T');
+        term.bgBlue(' T');
       } else if (array[i][j].value === 'TO') {
         term.bgYellow('TO');
       } else if (Number(array[i][j].value) === 0 && array[i][j].visibility === true) {
@@ -37,29 +34,9 @@ const labBackground = (array) => {
   return array;
 };
 
-const hintGen = () => {
-  let count = 0;
-  while (count < 7) {
-    let rng1 = Math.floor(Math.random() * 27 + 1);
-    let rng2 = Math.floor(Math.random() * 27 + 1);
-    for (let i = 0; i < array.length; i++) {
-      for (let j = 0; j < array[i].length; j++) {
-        if (rng1 === i && rng2 === j && array[i][j].visibility === false && Number(array[i][j].value) !== 1) {
-          array[i][j] = { value: 'H', visibility: true, discovered: false, decription: 'Here is the hint.' };
-          count++;
-        }
-      }
-    }
-  }
-  return array;
-};
 let life = { life: 10 };
 let torchLife = { life: 3 };
-hintGen();
-trapGen(array);
-foodGen(array);
-torch.torchGen(array, 3);
-labBackground(array);
+
 let a;
 let where = [1, 1];
 
